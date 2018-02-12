6b5c0c10e707b2c670eeec7eac66707a44c7449c
Alexander Porrello <alexporrello@gmail.com>
2018-02-12 15:27:06 -0600

    Code cleanup

--- src/log/LogWindow.java
+++ src/log/LogWindow.java
@@ -38,16 +38,11 @@ import ui.Tools;
 public class LogWindow extends JMPanel {
 	private static final long serialVersionUID = -1014191579466760962L;
 
-	private final int TICKET_DESCRIPTION = 0;
-	private final int DATE_CREATED = 1;
-	private final int JIRA_REPORT = 2;
-	private final int PART_NUM_32 = 3;
-	private final int PART_NUM_37 = 4;
-	private final int STATUS = 5;
-	private final int SPACER = 6;
-
 	/** All of the ticket files currently in existence on the user's computer **/
 	private ArrayList<ShortLog> shortLogs = new ArrayList<ShortLog>();
+	
+	/** The program body's main window **/
+	private MainWindow mw;
 
 	/** Determines how the tickets are sorted in {@link #logWindow} **/
 	private Compare compare = Compare.JIRA_TICKET_DESCRIPTION;
@@ -55,31 +50,17 @@ public class LogWindow extends JMPanel {
 	/** Determines if the tickets are sorted from back to front or front to back **/
 	private Boolean invert = false;
 
-	/** The labels that the user clicks to sort the tickets **/
-	private JLabel[] infoLabels = {
-			new JLabelEdge("Jira Ticket Description"),
-			new JLabelEdge("Date Created"),
-			new JLabelEdge("Jira Report"),
-			new JLabelEdge("32 Part Number"),
-			new JLabelEdge("37 Part Number"),
-			new JLabelEdge("Status", false),
-			new JLabelEdge("", false)
-	};
-
 	/** The JScrollPane that holds {@link #logWindow} **/
 	private JScrollPane logDialogScroll;
 
 	/** The panel upon which all the logs are displayed **/
 	private LogPanel logWindow = new LogPanel();
 
-	/** The program body's main window **/
-	private MainWindow mw;
-
+	
 	public LogWindow(MainWindow mw) {
 		this.mw = mw;
 
 		setupScrollPane();
-		setupAllLabels();
 		setLayout(new GridBagLayout());
 		setBorder(
 				BorderFactory.createCompoundBorder(
@@ -88,9 +69,9 @@ public class LogWindow extends JMPanel {
 								BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1),
 								BorderFactory.createEmptyBorder(5, 5, 5, 5))));
 
-		add(createInfoPanel(), new GridBagConstraints(0, 0, 1, 1, 1.0, 0.0, GridBagConstraints.NORTH, 
+		add(new HeadingPanel(), new GridBagConstraints(0, 0, 1, 1, 1.0, 0.0, GridBagConstraints.NORTH, 
 				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-		add(logDialogScroll,   new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, 
+		add(logDialogScroll,    new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.NORTH, 
 				GridBagConstraints.BOTH,       new Insets(0, 0, 0, 0), 0, 0));
 	}
 
@@ -104,73 +85,104 @@ public class LogWindow extends JMPanel {
 		logDialogScroll.setPreferredSize(new Dimension(900,150));
 	}
 
-	/** Sets up the JLabels that sort the displayed log entries **/
-	private void setupAllLabels() {
-		setupJLabel(DATE_CREATED, ShortLog.CREATED_DATE_SIZE, Compare.DATE_CREATED);
-		setupJLabel(JIRA_REPORT,  ShortLog.JIRA_REPORT_SIZE,  Compare.JIRA_REPORT);
-		setupJLabel(PART_NUM_32,  ShortLog.PART_NUM_32_SIZE,  Compare.PART_NUM_32);
-		setupJLabel(PART_NUM_37,  ShortLog.PART_NUM_37_SIZE,  Compare.PART_NUM_37);
-		setupJLabel(STATUS,       ShortLog.STATUS_SIZE,       Compare.STATUS);
-		setupJLabel(SPACER,       new Dimension(25, 24),      Compare.NULL);
+	/**
+	 * The panel upon which all of the headings are displayed.
+	 * @author Alexander Porrello
+	 */
+	private class HeadingPanel extends JMPanel {
+		private static final long serialVersionUID = -1040672906315782425L;
+		
+		/** The labels that the user clicks to sort the tickets **/
+		private JLabel[] headings = {
+				new JLabelEdge("Jira Ticket Description"),
+				new JLabelEdge("Date Created"),
+				new JLabelEdge("Jira Report"),
+				new JLabelEdge("32 Part Number"),
+				new JLabelEdge("37 Part Number"),
+				new JLabelEdge("Status", false),
+				new JLabelEdge("", false)
+		};
 
-		infoLabels[TICKET_DESCRIPTION].setBorder(BorderFactory.createEmptyBorder(10,0,10,0));
-		addActionListenerToJLabel(TICKET_DESCRIPTION, Compare.JIRA_TICKET_DESCRIPTION);
-	}
+		public HeadingPanel() {
+			setLayout(new GridBagLayout());
 
-	/** Sets up individual JLabels that the user clicks to sort the log entries **/
-	private void setupJLabel(int label, Dimension size, Compare thisCompare) {
-		infoLabels[label].setBorder(BorderFactory.createEmptyBorder(10,10,10,0));
-		infoLabels[label].setPreferredSize(size);
-		addActionListenerToJLabel(label, thisCompare);
-	}
+			setupAllLabels();
 
-	private void addActionListenerToJLabel(int label, Compare thisCompare) {
-		infoLabels[label].addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				infoLabels[label].setBackground(Color.decode("#D9EBF9"));
+			add(headings[HF.TICKET_DESCRIPTION], Tools.createGBC(1, 0, 1.0, new Insets(0,0,-1,0)));
+			for(int i = 1; i <= 6; i++) {
+				add(headings[i], Tools.createGBC(i+1, 0, 0.0, new Insets(0,0,-1,0)));
 			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				infoLabels[label].setBackground(new JLabel().getBackground());
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				infoLabels[label].setBackground(Color.decode("#BCDCF4"));
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				infoLabels[label].setBackground(new JLabel().getBackground());
-
-				if(infoLabels[label].contains(e.getPoint()) && compare != Compare.NULL) {
-					if(compare == thisCompare) {
-						invert = !invert;
-					} else {
-						compare = thisCompare;
-						invert = false;
-					}
-
-					logWindow.addAllToLogEntryPanel();
-				}
-			}
-		});
-	}
-
-	/** Sets up the info panel that displays the JLabels that the user clicks to sort log entries **/
-	private JMPanel createInfoPanel() {
-		JMPanel infoPanel = new JMPanel();
-
-		infoPanel.setLayout(new GridBagLayout());
-		infoPanel.add(infoLabels[TICKET_DESCRIPTION], Tools.createGBC(1, 0, 1.0, new Insets(0,0,-1,0)));
-
-		for(int i = 1; i <= 6; i++) {
-			infoPanel.add(infoLabels[i], Tools.createGBC(i+1, 0, 0.0, new Insets(0,0,-1,0)));
 		}
 
-		return infoPanel;
+		/** Sets up the JLabels that sort the displayed log entries **/
+		private void setupAllLabels() {
+			setupJLabel(HF.DATE_CREATED, ShortLog.CREATED_DATE_SIZE, Compare.DATE_CREATED);
+			setupJLabel(HF.JIRA_REPORT,  ShortLog.JIRA_REPORT_SIZE,  Compare.JIRA_REPORT);
+			setupJLabel(HF.PART_NUM_32,  ShortLog.PART_NUM_32_SIZE,  Compare.PART_NUM_32);
+			setupJLabel(HF.PART_NUM_37,  ShortLog.PART_NUM_37_SIZE,  Compare.PART_NUM_37);
+			setupJLabel(HF.STATUS,       ShortLog.STATUS_SIZE,       Compare.STATUS);
+			setupJLabel(HF.SPACER,       new Dimension(25, 24),      Compare.NULL);
+
+			headings[HF.TICKET_DESCRIPTION].setBorder(BorderFactory.createEmptyBorder(10,0,10,0));
+			addActionListenerToJLabel(HF.TICKET_DESCRIPTION, Compare.JIRA_TICKET_DESCRIPTION);
+		}
+
+		/** Sets up individual JLabels that the user clicks to sort the log entries **/
+		private void setupJLabel(int label, Dimension size, Compare thisCompare) {
+			headings[label].setBorder(BorderFactory.createEmptyBorder(10,10,10,0));
+			headings[label].setPreferredSize(size);
+			addActionListenerToJLabel(label, thisCompare);
+		}
+
+		/**
+		 * Implements highlighting with hover and click and sorting feature.
+		 * @param label the label to be processed.
+		 * @param thisCompare sets the comparison method.
+		 */
+		private void addActionListenerToJLabel(int label, Compare thisCompare) {
+			headings[label].addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					headings[label].setBackground(Color.decode("#D9EBF9"));
+				}
+
+				@Override
+				public void mouseExited(MouseEvent e) {
+					headings[label].setBackground(new JLabel().getBackground());
+				}
+
+				@Override
+				public void mousePressed(MouseEvent e) {
+					headings[label].setBackground(Color.decode("#BCDCF4"));
+				}
+
+				@Override
+				public void mouseReleased(MouseEvent e) {
+					headings[label].setBackground(new JLabel().getBackground());
+
+					if(headings[label].contains(e.getPoint()) && compare != Compare.NULL) {
+						if(compare == thisCompare) {
+							invert = !invert;
+						} else {
+							compare = thisCompare;
+							invert = false;
+						}
+
+						logWindow.addAllToLogEntryPanel();
+					}
+				}
+			});
+		}
+		
+		private class HF {
+			public static final int TICKET_DESCRIPTION = 0;
+			public static final int DATE_CREATED = 1;
+			public static final int JIRA_REPORT = 2;
+			public static final int PART_NUM_32 = 3;
+			public static final int PART_NUM_37 = 4;
+			public static final int STATUS = 5;
+			public static final int SPACER = 6;
+		}
 	}
 
 	/**
@@ -276,7 +288,7 @@ public class LogWindow extends JMPanel {
 					}
 				});
 			}
-			
+
 			return toReturn;
 		}
 	}
